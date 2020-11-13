6e6d3d612d304d220fb994b6354f9de00f44065e
이희제 <holim1226@gmail.com>
2020-11-14 02:37:48 +0900

    MERN_Practice

---
+++ MERN_Practice/practice/backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`server is running on ${port}`);
+});
